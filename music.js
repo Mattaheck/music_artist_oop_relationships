2e619eb518620852aa9e4674b5d3c71071bff8f7
Matt Heck <matthewaheck7u@gmail.com>
2017-03-26 19:53:01 -0500

    homework for object oriented

--- music.js
+++ music.js
@@ -1,12 +1,15 @@
 // Define the Artist constructor function here, with a 'name' property that can be set at instantiation,
 // and a 'songs' property that is an empty array to start
-function Artist() {
-
+function Artist(name) {
+this.name = name;
+this.songs = [];
 }
 
 // Define the Song constructor function here, with 'title' and 'artist' properties that can be set at instantiation,
 // and a 'playCount' property that is set to 0
-function Song() {
+function Song(title, Artist) {
+playCount === 0;
+this.songs(push)artist;
 
   // When a new song is instantiated, you'll want to add that song to its artist's 'songs' array.
   // You'll probably want to do that in here somewhere...
